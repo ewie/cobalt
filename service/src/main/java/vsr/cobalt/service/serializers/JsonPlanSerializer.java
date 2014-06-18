104ab0ca5590c43d25227d57fd212b8542d30517
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2014-06-18 21:01:26 +0200

    fix copyright header in various files

--- service/src/main/java/vsr/cobalt/service/serializers/JsonPlanSerializer.java
+++ service/src/main/java/vsr/cobalt/service/serializers/JsonPlanSerializer.java
@@ -1,9 +1,9 @@
 /*
-* Copyright (c) 2014, Erik Wienhold
-* All rights reserved.
-*
-* Licensed under the BSD 3-Clause License.
-*/
+ * Copyright (c) 2014, Erik Wienhold
+ * All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License.
+ */
 
 package vsr.cobalt.service.serializers;
 
