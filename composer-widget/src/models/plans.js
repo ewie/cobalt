9d7f2b5ea17b0fc26dccb5ce04e5c5db9b597390
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2014-08-25 23:16:26 +0200

    fix relative plan rating

--- composer-widget/src/models/plans.js
+++ composer-widget/src/models/plans.js
@@ -9,11 +9,27 @@
 
 function setRelativeRating(plans) {
   if (plans.length > 0) {
-    var minRating = plans[0].get('rating');
-    var maxRating = plans[plans.length - 1].get('rating');
-    plans.forEach(function (plan) {
-      plan.set({ relativeRating: 1 - ((plan.get('rating') - minRating) / (maxRating - minRating)) });
-    });
+    // Plans are ordered by rating, therefore we know the minimum and maximum
+    // without searching.
+    var min = plans[0].get('rating');
+    var max = plans[plans.length - 1].get('rating');
+
+    var dr = max - min;
+
+    if (dr === 0) {
+      // The min and max may be zero causing division by zero.
+      // In this case, however, every plan has equal rating, thus a relative
+      // rating of 100%.
+      plans.forEach(function (plan) {
+        plan.set({ relativeRating: 1 });
+      });
+    } else {
+      plans.forEach(function (plan) {
+        var r = plan.get('rating');
+        var rr = 1 - ((r - min) / dr);
+        plan.set({ relativeRating: rr });
+      });
+    }
   }
 }
 
