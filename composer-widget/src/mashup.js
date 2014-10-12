ewie/cobalt

commit 11baa956cd7d5bd2b74b7fdfd2cceaf6c08d7f25

fix occasional server errors during widget loading
Fix sequential execution of deferred to actually be sequential (wait
until current deferred is done).
Remove optional delay when adding or removing widgets from mashup.
This delay is no longer necessary when adding and removing widgets is
properly performed sequentially.

composer-widget/src/mashup.js
@@ -81,16 +81,16 @@ function loadWidgets(execution) {
       var fns = instances.map(function (instance) {
         return function () {
           return rave.addWookieWidgetToPage(instance.wookie)
-            .then(function (regionWidget) {
+            .done(function (regionWidget) {
               instance.region = regionWidget;
               return instance;
             });
         };
       });
 
-      // add the widgets one after another with a delay in between, otherwise
-      // the request may happen to quickly for Rave to handle
-      return $.Deferred.sequence(fns, 500);
+      // add the widgets one after another, otherwise the requests may happen
+      // to quickly for Rave to handle
+      return $.Deferred.sequence(fns);
 
     });
 }

composer-widget/src/main.js
@@ -140,24 +140,22 @@ $.Deferred.sequence = function (fns, delay) {
   (function next(index, promise) {
     // create the next promise and push its result when available
     var p = fns[index]()
-      .then(function (r) {
+      .fail(function (r) { dfd.reject(r) })
+      .done(function (r) {
         results.push(r);
         if (index === lastIndex) {
           dfd.resolve(results);
+        } else {
+          setTimeout(function () {
+            next(index + 1, promise);
+          }, delay);
         }
       });
 
     // set up the promise for the next step, either chain on the promise from
     // the previous step or use the promise from the current step (the very
     // first step)
-    promise = promise ? promise.then(p) : p;
-
-    if (index < lastIndex) {
-      // delay the next step
-      setTimeout(function () {
-        next(index + 1, promise);
-      }, delay);
-    }
+    promise = promise ? promise.done(p) : p;
   }(0));
 
   return dfd.promise();

composer-widget/src/rave.js
@@ -79,7 +79,8 @@ function addWookieWidgetToPage(widget) {
       widgetId: widget.id,
       successCallback: function (id) {
         renderWidget(id)
-          .then(function (r) { dfd.resolve(r) });
+          .done(function (r) { dfd.resolve(r) })
+          .fail(function (r) { dfd.reject(r) });
       }
     });
   });
@@ -108,9 +109,16 @@ function renderWidget(id) {
     var _load = parent.$.fn.load;
 
     _$.fn.load = function (url, callback) {
-      _load.call(this, url, function () {
-        callback();
-        dfd.resolve(getRegionWidget(id));
+      _load.call(this, url, function (response, status, xhr) {
+        try {
+          callback();
+        } catch (e) {
+          dfd.reject();
+          return;
+        }
+        xhr
+          .done(function () { dfd.resolve(getRegionWidget(id)) })
+          .fail(function () { dfd.reject() });
       });
     };
 
@@ -189,7 +197,7 @@ function clearRegionWidgets() {
         return removeRegionWidget(widget);
       };
     });
-  return $.Deferred.sequence(fns, 200);
+  return $.Deferred.sequence(fns);
 }
 
 

commit 686bd728bb3f3ae3273482b2639b2880de35183e

fix instance collection during widget loading
Fix bug introduced with 11baa956cd7d5bd2b74b7fdfd2cceaf6c08d7f25

composer-widget/src/mashup.js
@@ -81,7 +81,7 @@ function loadWidgets(execution) {
       var fns = instances.map(function (instance) {
         return function () {
           return rave.addWookieWidgetToPage(instance.wookie)
-            .done(function (regionWidget) {
+            .then(function (regionWidget) {
               instance.region = regionWidget;
               return instance;
             });